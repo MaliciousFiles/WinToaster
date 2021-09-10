d314febb2e5ecd3d944fb7cf8b1ed6555380ffbc
MaliciousFiles <mtroalson@gmail.com>
2021-09-10 09:38:11 -0500

    Update version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/MaliciousFiles/WinToaster/'
 EMAIL = 'mtroalson@gmail.com'
 AUTHOR = 'Malcolm Roalson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
